ebbbffa4d56fa124521323adeb331b4613a1a6ed
jpranto2 <jpranto2@gmail.com>
2023-09-12 09:42:52 +0600

    fixed the dropdown

--- src/test/java/components/DropDown.java
+++ src/test/java/components/DropDown.java
@@ -21,7 +21,15 @@ public class DropDown extends LoginSteps {
         }
         driver.findElement(By.xpath("//*[@id=\'demo-customized-button\']")).click();
         //expand the OPTIONS button
-        driver.findElement(By.xpath("//li[normalize-space()='Duplicate']")).click();
+        String expectedText= "Duplicate";
+        String actualText= driver.findElement(By.xpath("//li[normalize-space()='Duplicate']")).getText();
+        if (expectedText.equals(actualText)){
+            System.out.println("Duplicate option is matching!");
+        }else{
+            System.out.println("Duplicate option is not matching!");
+        }
+
+        driver.quit();
 
 
     }
